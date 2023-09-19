b8feb7ff3b398ccdf04e2912ccd0ca977f4722a7
tfosra <souleymanr@gmail.com>
2023-09-19 11:51:19 +0100

    Webserver added

---
+++ vagrant/webserver.py
@@ -0,0 +1,30 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>Hello!</body></html>"
+                self.wfile.write(output)
+                print(output)
+                return
+        except IOError:
+            self.send_error(404, "File not found %s" % self.path)
+
+def main():
+    try:
+        port = 8081
+        server = HTTPServer(("", port), webserverHandler)
+        print("Webserver running on port %s" % port)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("^C entered, stopping web server...")
+        server.socket.close()
+        print("Server stopped.")
+
+if __name__ == '__main__':
+    main()
